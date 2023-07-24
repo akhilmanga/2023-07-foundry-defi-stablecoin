b9bba94055bed53b8c1b8a45c3376f7d4abaa1ea
AKHIL MANGA <116151859+akhilmanga@users.noreply.github.com>
2023-07-24 18:49:28 +0530

    Update OracleLib.sol

--- src/libraries/OracleLib.sol
+++ src/libraries/OracleLib.sol
@@ -26,10 +26,6 @@ library OracleLib {
         (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound) =
             priceFeed.latestRoundData();
 
-if (updatedAt == 0 || answeredInRound < roundId) {
-            revert OracleLib__StalePrice();
-        }
-
         uint256 secondsSince = block.timestamp - updatedAt;
         if (secondsSince > TIMEOUT) revert OracleLib__StalePrice();
 
